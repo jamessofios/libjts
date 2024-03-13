1bb595145f4f0fb1cd560a8bb5b318f192ee1bc3
James Sofios <jamessofios@gmail.com>
2024-03-12 21:38:09 -0500

    Added my own iso646 header file

---
+++ jts_iso646.h
@@ -0,0 +1,47 @@
+/*
+If the macros of iso646.h are not already defined, then I define them myself
+*/
+
+#ifndef and
+	#define and &&
+#endif
+
+#ifndef and_eq
+	#define and_eq &=
+#endif
+
+#ifndef bitand
+	#define bitand &
+#endif
+
+#ifndef bitor
+	#define bitor |
+#endif
+
+#ifndef compl
+	#define compl ~
+#endif
+
+#ifndef not
+	#define not !
+#endif
+
+#ifndef
+	#define not_eq !=
+#endif
+
+#ifndef or
+	#define or ||
+#endif
+
+#ifndef or_eq
+	#define or_eq |=
+#endif
+
+#ifndef xor
+	#define xor ^
+#endif
+
+#ifndef xor_eq
+	#define xor_eq ^=
+#endif
